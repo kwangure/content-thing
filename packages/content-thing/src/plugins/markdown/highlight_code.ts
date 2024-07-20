6027db0d25d460e916b4efe20306240c5dc7795f
kwangure <kaf.wangure@gmail.com>
2024-07-20 15:06:02 +0300

    fix: highlight code with no lang

--- packages/content-thing/src/plugins/markdown/highlight_code.ts
+++ packages/content-thing/src/plugins/markdown/highlight_code.ts
@@ -29,20 +29,18 @@ export async function highlightCodeBlocks(tree: Root) {
 		}
 	});
 
-	const highlighters = new Map<string | undefined | null, Highlighter>();
+	const highlighterCache = new Map<string | undefined | null, Highlighter>();
 	const promises = codeBlocks.map(async (node) => {
 		const lang = node.type === 'code' ? node.lang : node.data?.attributes.lang;
-		if (typeof lang === 'string') {
-			let highlighter = highlighters.get(lang);
-			if (!highlighter) {
-				highlighter = await getSupportedHighlighter(lang);
-				highlighters.set(lang, highlighter);
-			}
-			if (node.type === 'code') {
-				node.data.highlightedLines = highlightLines(node.value, highlighter);
-			} else {
-				node.data.highlightedTokens = highlighter(node.value);
-			}
+		let highlighter = highlighterCache.get(lang);
+		if (!highlighter) {
+			highlighter = await getSupportedHighlighter(lang);
+			highlighterCache.set(lang, highlighter);
+		}
+		if (node.type === 'code') {
+			node.data.highlightedLines = highlightLines(node.value, highlighter);
+		} else {
+			node.data.highlightedTokens = highlighter(node.value);
 		}
 	});
 	await Promise.all(promises);
