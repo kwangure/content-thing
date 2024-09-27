c5d14f315268819785098f46d73a76f332eb6e92
kwangure <kaf.wangure@gmail.com>
2024-09-28 01:12:07 +0300

    feat: add line ranges to vite-plugin-syntax-highlight

--- packages/vite-plugin-syntax-highlighter/src/index.ts
+++ packages/vite-plugin-syntax-highlighter/src/index.ts
@@ -4,11 +4,14 @@ import {
 	highlightLines,
 	isSupportedLanguage,
 } from '@content-thing/syntax-highlighter';
+import { parseRanges, selectLines } from '@content-thing/line-range';
 import fs from 'node:fs';
 
 const EXTENSION_REGEX = /\.([^/.?#]+)(?=\?|#|$)/;
 const HIGHLIGHT_REGEX = /(\?|&)highlight(?:&|$)/;
 const LINES_REGEX = /(\?|&)lines(?:&|$)/;
+const NEW_LINE_REGEX = /\r\n|\r|\n/;
+const RANGE_REGEX = /(\?|&)range=([\d,-]+)(?:&|$)/;
 const POSTFIX_REGEX = /[?#].*$/;
 function cleanUrl(url: string): string {
 	return url.replace(POSTFIX_REGEX, '');
@@ -26,6 +29,15 @@ export function syntaxHighlighter() {
 		configResolved(server) {
 			vite = server;
 		},
+		async resolveId(id, importer, options) {
+			if (id.match(RANGE_REGEX)) {
+				const [path, query] = id.split('?');
+				const resolution = await this.resolve(path, importer, options);
+				if (resolution) {
+					return resolution.id + '?' + query;
+				}
+			}
+		},
 		async load(id) {
 			if (!id.match(HIGHLIGHT_REGEX)) return;
 
@@ -33,7 +45,6 @@ export function syntaxHighlighter() {
 			if (!match || typeof match[1] !== 'string') return;
 
 			const extension = match[1];
-
 			if (!isSupportedLanguage(extension)) {
 				const message = `Unable to syntax highlight ${JSON.stringify(id)}. Language "${extension}" is unsupported.`;
 				if (vite.command === 'build') {
@@ -44,8 +55,17 @@ export function syntaxHighlighter() {
 				return;
 			}
 
+			let code = fs.readFileSync(cleanUrl(id), 'utf-8');
+
+			const rangeMatch = id.match(RANGE_REGEX);
+			if (rangeMatch) {
+				const lines = code.split(NEW_LINE_REGEX);
+				const rangeString = rangeMatch[2];
+				const ranges = parseRanges(rangeString, lines.length);
+				code = selectLines(lines, ranges).join('\n');
+			}
+
 			const highlighter = await getHighlighter(extension);
-			const code = fs.readFileSync(cleanUrl(id), 'utf-8');
 			const highlightResult = LINES_REGEX.test(id)
 				? highlightLines(code, highlighter)
 				: highlighter(code);
