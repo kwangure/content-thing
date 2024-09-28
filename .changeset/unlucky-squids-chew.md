f3c08ff268d0bb726ffeaa58738d8c3ad922032c
kwangure <kaf.wangure@gmail.com>
2024-09-28 18:01:40 +0300

    fix: use record-like type for data

---
+++ .changeset/unlucky-squids-chew.md
@@ -0,0 +1,5 @@
+---
+'content-thing': patch
+---
+
+Use record-like type for data
--- packages/content-thing/src/plugins/utils/stringify.ts
+++ packages/content-thing/src/plugins/utils/stringify.ts
@@ -9,7 +9,7 @@ export function stringifyData(record: Record<string, unknown>) {
 
 export function stringifyTypes(collectionConfig: CollectionConfig) {
 	let code = '// This file is auto-generated. Do not edit directly.\n';
-	code += `export interface Frontmatter {\n`;
+	code += `export type Frontmatter = {\n`;
 	const fields = Object.entries(collectionConfig.fields);
 	for (const [fieldName, field] of fields) {
 		const type = 'typeScriptType' in field ? field.typeScriptType : field.type;
